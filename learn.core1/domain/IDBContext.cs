c1971a086df506ac5c391e6838a6292becbb3909
montheraljodeh <user@DESKTOP-HR1O67U>
2022-06-28 11:07:46 +0300

    added

---
+++ learn.core1/domain/IDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace learn.core1.domain
+{
+    public interface IDBContext
+    {
+        public DbConnection dbConnection { get; }
+    }
+}
