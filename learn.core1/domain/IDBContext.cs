7bbae5a7b5b20a87a128268b631fc9ac1089dce4
montheraljodeh <user@DESKTOP-HR1O67U>
2022-06-29 09:41:30 +0300

    chagedit

--- learn.core1/domain/IDBContext.cs
+++ learn.core1/domain/IDBContext.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace learn.core1.domain
 {
-    public interface IDBContext
+   public  interface IDBContext
     {
         public DbConnection dbConnection { get; }
     }
---
+++ learn.infra1/domain/DbContext.cs
@@ -0,0 +1,49 @@
+﻿using learn.core1.domain;
+using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace learn.infra1.domain
+{
+    public class DbContext : IDBContext
+    {
+        private DbConnection connection;
+        private IConfiguration configuration;
+
+
+        /*when execute project we will inialize value by constructor */
+        public DbContext(DbConnection connection, IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public DbConnection dbConnection
+        {/*
+          check connection if null or nut if (connection==null)
+            check connection if close () i want open connection string 
+
+            new oracleconnection(configuration)this is get connection string 
+            in appsettings.json
+          
+          
+          */
+            get
+            {
+                if(connection==null)
+                {
+
+                    connection = new OracleConnection(configuration["ConnectionStrings:DBConnectionString"]);
+
+                    connection.Open();
+                }else if (connection.State !=System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                return connection;
+            }
+        }
+    }
+}
