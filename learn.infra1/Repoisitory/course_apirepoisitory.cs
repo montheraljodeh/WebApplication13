eb05556bff4c01147d00468626c6227cf414037f
montheraljodeh <user@DESKTOP-HR1O67U>
2022-06-30 09:48:57 +0300

    added

---
+++ learn.infra1/Repoisitory/course_apirepoisitory.cs
@@ -0,0 +1,26 @@
+﻿using learn.core1.Data;
+using learn.core1.Repoisitory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learn.infra1.Repoisitory
+{
+    public class course_apirepoisitory : Icourse_apirepository
+    {
+        public bool deletecoure(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<course_api> getallcourse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool insertcourse(course_api course)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
